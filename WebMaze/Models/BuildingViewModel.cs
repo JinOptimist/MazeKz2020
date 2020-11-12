4296aa21c43c98ee88a8663cb836189cf023937b
Daryn <akhmetovdaryn@gmail.com>
2020-11-12 13:37:33 +0600

    Change StudentController.cs and _Layout.cshtml. Add BuildingViewModel.cs and Akhmetov.cshtml.

---
+++ WebMaze/Models/BuildingViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebMaze.Models
+{
+    public class Building
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Height { get; set; }
+    }
+}
