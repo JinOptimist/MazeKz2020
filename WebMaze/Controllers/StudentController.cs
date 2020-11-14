0f85973e69ba6f7c392c059a114732cd126649bf
Boris Korobeinikov <slon2k@gmail.com>
2020-11-14 21:43:58 +0600

    fixed Akhmetov Action

--- WebMaze/Controllers/StudentController.cs
+++ WebMaze/Controllers/StudentController.cs
@@ -498,6 +498,56 @@ namespace WebMaze.Controllers
             return View(models);
         }
 
+        public IActionResult Akhmetov()
+        {
+            var buildings = new List<BuildingViewModel>() {
+                new BuildingViewModel
+                {
+                    Name = "Бурдж-Халифа",
+                    Description = "Cверхвысотный небоскрёб высотой 828 метров в Дубае (ОАЭ), самое высокое и самое многоэтажное здание в мире.",
+                    Url = "https://kuku.travel/wp-content/uploads/2018/07/%D0%9D%D0%B5%D0%B1%D0%BE%D1%81%D0%BA%D1%80%D0%B5%D0%B1-%D0%91%D1%83%D1%80%D0%B4%D0%B6-%D0%A5%D0%B0%D0%BB%D0%B8%D1%84%D0%B0-%D0%B2-%D0%94%D1%83%D0%B1%D0%B0%D0%B5.jpg",
+                    Height = 828
+                },
+                new BuildingViewModel
+                {
+                    Name = "Шанхайская башня",
+                    Description = "Cверхвысокое здание в районе Пудун города Шанхай в Китае.",
+                    Url = "https://www.votpusk.ru/gallery/mobile/132311.jpg",
+                    Height = 632
+                },
+                new BuildingViewModel
+                {
+                    Name = "Часовая королевская башня",
+                    Description = "Расположена в городе Мекка, Саудовская Аравия.",
+                    Url = "https://islamosfera.ru/wp-content/uploads/2019/12/%D0%90%D0%B1%D1%80%D0%B0%D0%B4%D0%B6-%D0%B0%D0%BB%D1%8C-%D0%91%D0%B5%D0%B9%D1%82-1024x683.jpg",
+                    Height = 601
+                },
+                new BuildingViewModel
+                {
+                    Name = "Международный финансовый центр Пинань",
+                    Description = "Комплекс зданий, включающий 599-метровый, 115-этажный небоскрёб в городе Шэньчжэнь, провинция Гуандун, Китай.",
+                    Url = "https://images.adsttc.com/media/images/5a4c/6cb4/b22e/38fb/6600/00e0/large_jpg/3_Ping_An_FC_(c)_Tim_Griffith.jpg?1514957995",
+                    Height = 599
+                },
+                new BuildingViewModel
+                {
+                    Name = "Lotte World Tower",
+                    Description = "123-этажный сверхвысокий небоскрёб, в районе развлекательного комплекса Lotte World в Сеуле, Южная Корея.",
+                    Url = "https://i.insider.com/5c12c950a040ff03023de2c9?width=1100&format=jpeg&auto=webp",
+                    Height = 555
+                },
+                new BuildingViewModel
+                {
+                    Name = "Всемирный торговый центр 1",
+                    Description = "Центральное здание в новом комплексе Всемирного торгового центра в нижнем Манхэттене (город Нью-Йорк, США).",
+                    Url = "https://aecom.com/wp-content/uploads/2015/10/1WTC_Credit-Michael-Mahesh-PANYNJ-810x531.jpg",
+                    Height = 541
+                }
+            };
+
+            return View(buildings.OrderByDescending(building => building.Height).ToList());
+        }
+
         public IActionResult Boris()
         {
             var mazeGenerator = new MazeGenerator();
