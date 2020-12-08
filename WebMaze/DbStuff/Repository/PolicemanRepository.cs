3b91e7f03422a6817e77e1c41699e83a31ca5532
srazhov <65289085+srazhov@users.noreply.github.com>
2020-12-08 20:54:35 +0600

    Added new table Policeman

---
+++ WebMaze/DbStuff/Repository/PolicemanRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMaze.DbStuff.Model.Police;
+
+namespace WebMaze.DbStuff.Repository
+{
+    public class PolicemanRepository : BaseRepository<Policeman>
+    {
+        public PolicemanRepository(WebMazeContext context) : base(context) { }
+    }
+}
--- WebMaze/Migrations/20201208145236_Added policeman.Designer.cs
+++ WebMaze/Migrations/20201208145236_Added policeman.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMaze.DbStuff;
 
 namespace WebMaze.Migrations
 {
     [DbContext(typeof(WebMazeContext))]
-    partial class WebMazeContextModelSnapshot : ModelSnapshot
+    [Migration("20201208145236_Added policeman")]
+    partial class Addedpoliceman
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
